71187c0760ec32036b852ee6ddd3a27a3d1f3537
Vibhav S <v@vsinha.me>
2021-06-02 23:53:43 +0530

    added matrix for all requirements and levels

---
+++ iframe/support-matrix.js
@@ -0,0 +1,47 @@
+console.log('this is the support matrix');
+const robustnessLevels = [
+  '',
+  'SW_SECURE_CRYPTO',
+  'SW_SECURE_DECODE',
+  'HW_SECURE_CRYPTO',
+  'HW_SECURE_DECODE',
+  'HW_SECURE_ALL'
+];
+const keysRequirementDict = ['required', 'optional', 'not-allowed'];
+const matrixBody = document.querySelector('#matrixBody');
+
+function generateRow(label) {
+  const tr = document.createElement('tr');
+  const headCell = document.createElement('td');
+  headCell.innerText = label;
+  tr.appendChild(headCell);
+  for (const requirement of keysRequirementDict) {
+    const cell = document.createElement('td');
+    cell.className = requirement;
+    tr.appendChild(cell);
+  }
+  return tr;
+}
+
+for (const level of robustnessLevels) {
+  const row = generateRow(level);
+  matrixBody.appendChild(row);
+  for (const requirement of keysRequirementDict) {
+    navigator.requestMediaKeySystemAccess('com.widevine.alpha', [{
+      initDataTypes: ['cenc'],
+      distinctiveIdentifier: requirement,
+      videoCapabilities: [{
+        contentType: 'video/mp4;codecs="avc1.42E01E"',
+        robustness: level,
+      }]
+    }]).then(() => {
+      console.log('Success for ', {level, requirement});
+      row.getElementsByClassName(requirement)[0].innerText = 'success';
+    }).catch((e) => {
+      console.log('Failure for ', {level, requirement});
+      const cell = row.getElementsByClassName(requirement)[0];
+      cell.innerText = 'failure';
+      cell.setAttribute('title', e.message)
+    });
+  }
+}
